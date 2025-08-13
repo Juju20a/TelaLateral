a24705d6f01205729914af5f83925735697789d1
Juju20a <juliene.barbosa@academico.ifpb.edu.br>
2025-08-13 09:46:52 -0300

    DrawerLayout.tsx

--- ifence-mobile/app/DrawerLayout.tsx
+++ ifence-mobile/app/DrawerLayout.tsx
@@ -1,1 +1,25 @@
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Home from "@/screens/Home";
+import AdicionarPulseiraScreen from "@/screens/AdicionarPulseiraScreen";
+import ListarCercas from "@/screens/ListarCercas";
+import Alarme from "@/screens/Alarme";
+
+const Drawer = createDrawerNavigator();
+
+export default function DrawerLayout() {
+  return (
+    <Drawer.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: true,
+      }}
+    >
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Adicionar Pulseira" component={AdicionarPulseiraScreen} />
+      <Drawer.Screen name="Listar Cercas" component={ListarCercas} />
+      <Drawer.Screen name="Monitorar Cercas" component={Alarme} />
+    </Drawer.Navigator>
+  );
+}
 
